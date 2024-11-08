12949c2dc45190db8246a8978709afbcdd2d6b77
ChanGyun Lee <lgyn10@naver.com>
2024-11-08 14:38:08 +0900

    step 1: createElement 함수 적용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
+//! createElement
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => (typeof child === 'object' ? child : createTextElement(child))),
+    },
+  };
+}
+
+//! createTextElement
+// createElement에서 사용
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+//! ========================= 실행 코드 =========================
+
+const Didact = {
+  createElement,
+};
+
+/** @jsx Didact.createElement */
 const element = (
   <div id='foo'>
     <a>bar</a>
     <b />
   </div>
 );
+
 const container = document.getElementById('root');
 ReactDOM.render(element, container);
