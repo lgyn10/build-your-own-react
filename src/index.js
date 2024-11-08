3c54cd5262111b913e4c14a99cb84c35b01140bb
ChanGyun Lee <lgyn10@naver.com>
2024-11-08 14:43:54 +0900

    step 3: 동시성 모드 적용

--- src/index.js
+++ src/index.js
@@ -34,11 +34,28 @@ function render(element, container) {
     });
 
   element.props.children.forEach((child) => render(child, dom));
-
   container.appendChild(dom);
 }
 
+//! 동시성 모드 - Concurrent Mode
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
+
 //! ========================= 실행 코드 =========================
+
+requestIdleCallback(workLoop);
 
 const Didact = {
   createElement,
